78dceadf1f35ece1fdf67e0d2847a92a8ba4453c
KeneronAgrov <ingvarkoch@gmail.com>
2026-03-05 14:07:28 +0100

    added Menu fully functional with assets and SFX

---
+++ Minos/Assets/Script/LoadSceneByIndex.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneByIndex : MonoBehaviour {
+    // General method to load scenes based on build index
+    public void LoadScene(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
